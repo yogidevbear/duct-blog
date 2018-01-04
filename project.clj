d5403ba204300284c1465460ad21c7c9a8180c30
Andrew Jackson <Andrew Jackson>
2018-01-04 23:31:21 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,26 @@
+(defproject duct-blog "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [duct/core "0.6.1"]
+                 [duct/module.logging "0.3.1"]
+                 [duct/module.web "0.6.3"]
+                 [duct/module.ataraxy "0.2.0"]
+                 [duct/module.sql "0.4.2"]
+                 [org.xerial/sqlite-jdbc "3.20.1"]]
+  :plugins [[duct/lein-duct "0.10.5"]]
+  :main ^:skip-aot duct-blog.main
+  :resource-paths ["resources" "target/resources"]
+  :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]]
+  :profiles
+  {:dev  [:project/dev :profiles/dev]
+   :repl {:prep-tasks   ^:replace ["javac" "compile"]
+          :repl-options {:init-ns user}}
+   :uberjar {:aot :all}
+   :profiles/dev {}
+   :project/dev  {:source-paths   ["dev/src"]
+                  :resource-paths ["dev/resources"]
+                  :dependencies   [[integrant/repl "0.2.0"]
+                                   [eftest "0.4.0"]
+                                   [kerodon "0.9.0"]]}})
